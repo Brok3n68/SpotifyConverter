994ee48d6e15ceda95a4b7d480d8a81ffda951e4
Giumaggone <giuseppemagaro3@gmail.com>
2023-09-16 16:54:53 +0200

    mod 2

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fetchButton = document.getElementById("fetchButton");
-  const spotifyLinkInput = document.getElementById("spotifyLink");
+  const spotifyLinkInput = document.getElementById("link-box");
   const metadataResult = document.getElementById("metadataResult");
+  const userChoice = document.getElementById("choice");
 
   fetchButton.addEventListener("click", async () => {
     const spotifyLink = spotifyLinkInput.value;
 
+    fetchButton.remove();
+    spotifyLinkInput.remove();
+    userChoice.remove();
+
     // Regular expression to match a playlist
     const playlistIdMatch = spotifyLink.match(/playlist\/([a-zA-Z0-9]+)/);
     const trackIdMatch = spotifyLink.match(/track\/([a-zA-Z0-9]+)/);
+    const albumIdMatch = spotifyLink.match(/album\/([a-zA-Z0-9]+)/);
 
     if (playlistIdMatch) {
       // Handle playlist link
@@ -79,20 +85,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Now 'allTracks' contains all the tracks in the playlist
             // Print metadata for each track
-            metadataResult.innerHTML = "<h2>Metadata for Playlist Tracks:</h2>";
+            metadataResult.innerHTML = `<h2>Metadata for Playlist Tracks:</h2>
+              <button type=button id="downloadButton">Scarica La Playlist</button>`;
             allTracks.forEach((track, index) => {
               metadataResult.innerHTML += `
-                <h3>Track ${index + 1}:</h3>
-                <ul>
-                  <li><strong>Name:</strong> ${track.track.name}</li>
-                  <li><strong>Artist(s):</strong> ${track.track.artists
+                <h3>[${index + 1} / ${allTracks.length}]</h3>
+                  <p><strong>Name:</strong> ${track.track.name}</p>
+                  <p><strong>Artist(s):</strong> ${track.track.artists
                     .map((artist) => artist.name)
-                    .join(", ")}</li>
-                  <li><strong>Album:</strong> ${track.track.album.name}</li>
-                  <li><strong>Release Date:</strong> ${
+                    .join(", ")}</p>
+                  <p><strong>Album:</strong> ${track.track.album.name}</p>
+                  <p><strong>Release Date:</strong> ${
                     track.track.album.release_date
-                  }</li>
-                </ul>`;
+                  }</p>
+                  <br>
+                  <br>`;
             });
           } else {
             // Handle error for initial playlist request
@@ -109,7 +116,9 @@ document.addEventListener("DOMContentLoaded", () => {
           "<p>An error occurred while fetching metadata.</p>";
       }
     } else if (trackIdMatch) {
+
       // Handle single track link
+      
       const trackId = trackIdMatch[1];
       const apiUrl = `https://api.spotify.com/v1/tracks/${trackId}`;
 
@@ -151,16 +160,14 @@ document.addEventListener("DOMContentLoaded", () => {
             // Display metadata for the single track
             metadataResult.innerHTML = "<h2>Metadata for Single Track:</h2>";
             metadataResult.innerHTML += `
-              <ul>
-                <li><strong>Name:</strong> ${data.name}</li>
-                <li><strong>Artist(s):</strong> ${data.artists
+                <p><strong>Name:</strong> ${data.name}</p>
+                <p><strong>Artist(s):</strong> ${data.artists
                   .map((artist) => artist.name)
-                  .join(", ")}</li>
-                <li><strong>Album:</strong> ${data.album.name}</li>
-                <li><strong>Release Date:</strong> ${
+                  .join(", ")}</p>
+                <p><strong>Album:</strong> ${data.album.name}</p>
+                <p><strong>Release Date:</strong> ${
                   data.album.release_date
-                }</li>
-              </ul>`;
+                }</p>`;
           } else {
             metadataResult.innerHTML =
               "<p>Failed to fetch metadata. Please check the Spotify link and try again.</p>";
@@ -174,10 +181,95 @@ document.addEventListener("DOMContentLoaded", () => {
         metadataResult.innerHTML =
           "<p>An error occurred while fetching metadata.</p>";
       }
-    } else {
+    } else if (albumIdMatch){
+      const albumId = albumIdMatch[1];
+      const initialAlbumUrl = `https://api.spotify.com/v1/albums/${albumId}`;
+
+      // Your Spotify Developer App Client ID and Client Secret
+      const clientId = "65400afdb63f4a758b4daa4f4333a362";
+      const clientSecret = "d768613ce77841029180d17d5d78f239";
+
+      // Base64 encode the client ID and client secret
+      const basicAuth = btoa(`${clientId}:${clientSecret}`);
+
+      // Request an access token
+      try {
+        const tokenResponse = await fetch(
+          "https://accounts.spotify.com/api/token",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Basic ${basicAuth}`,
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: "grant_type=client_credentials", // For server-to-server authentication
+          }
+        );
+
+        if (tokenResponse.ok) {
+          const tokenData = await tokenResponse.json();
+          const accessToken = tokenData.access_token;
+
+          // Fetch the initial playlist data to get the total number of tracks
+          const initialPlaylistResponse = await fetch(initialAlbumUrl, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+
+          try {
+            const initialAlbumResponse = await fetch(initialAlbumUrl, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            });
+          
+            if (initialAlbumResponse.ok) {
+              const initialAlbumData = await initialAlbumResponse.json();
+              const albumName = initialAlbumData.name;
+              const artistNames = initialAlbumData.artists.map((artist) => artist.name);
+          
+              // Ottieni direttamente le tracce dell'album
+              const allTracks = initialAlbumData.tracks.items;
+          
+              metadataResult.innerHTML = `<h2>Metadata for Album: ${albumName} by ${artistNames.join(", ")}</h2>
+                <button type="button" id="downloadButton">Scarica L'Album</button>`;
+              
+                allTracks.forEach((track, index) => {
+                  metadataResult.innerHTML += `
+                    <h3>[${index + 1} / ${allTracks.length}]</h3>
+                    <p><strong>Name:</strong> ${track.name}</p>
+                    <p><strong>Artist(s):</strong> ${track.artists
+                      .map((artist) => artist.name)
+                      .join(", ")}</p>
+                    ${track.album ? `<p><strong>Album:</strong> ${track.album.name}</p>` : ''}
+                    ${track.album ? `<p><strong>Release Date:</strong> ${track.album.release_date}</p>` : ''}
+                    <br>
+                    <br>`;
+                });
+            } else {
+              metadataResult.innerHTML = "<p>Failed to fetch album data. Please check the Spotify album link and try again.</p>";
+            }
+          } catch (error) {
+            console.error(error);
+            metadataResult.innerHTML = "<p>An error occurred while fetching album metadata.</p>";
+          }
+        } else {
+          metadataResult.innerHTML =
+            "<p>Failed to obtain access token. Please check your credentials.</p>";
+        }
+      } catch (error) {
+        console.error(error);
+        metadataResult.innerHTML =
+          "<p>An error occurred while fetching metadata.</p>";
+      }
+    }
+    else {
       // Handle invalid link
       metadataResult.innerHTML =
         "<p>Invalid Spotify link. Please enter a valid Spotify track or playlist link.</p>";
     }
   });
+
+console.log();
 });
